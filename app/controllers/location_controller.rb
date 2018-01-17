b7a987df83251cf7f8227b0e1e0ba78f3355f7e2
Miguel Rust <hava12soft@gmail.com>
2018-01-16 19:31:23 -0700

    Fixed issues with the layout.

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,6 +1,6 @@
 class LocationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  layout 'application'
   def index
 
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  def authentication_helper
 end
