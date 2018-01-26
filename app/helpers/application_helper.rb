045a728480cba86705090ee902c0918ee63e7832
Miguel Rust <hava12soft@gmail.com>
2018-01-25 20:04:42 -0700

    Fixed authentication helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def authentication_helper
     if signed_in?
       message = "Welcome #{current_user.email}!"
-      content = message + (link_to 'Logout', destroy_user_session_path, method: :delete)
+      content = message + (link_to 'Logout', destroy_user_session_path, method: :delete) + (link_to 'Edit Acount', edit_user_registration_path)
       content.html_safe
     else
       link_to "Log In!", new_user_session_path
