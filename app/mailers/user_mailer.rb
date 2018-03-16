c402b8382aa1519fda25a95d1fe3403b36f8947a
Miguel Rust <hava12soft@gmail.com>
2018-03-15 18:46:56 -0600

    Configuring new email address.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,21 @@
 class UserMailer < ApplicationMailer
-  default from: 'miguel@prettygoodstudios.com'
-
+  default from: 'info@geofoc.us'
   def verify_email(user)
     @user = User.find(user)
+    @root_url = root_url
     mail(to: @user.email, subject: 'Geofocus Email Verification')
   end
   def new_report(report)
     @report = Report.find(report)
     @recipients = User.all.where(role: "admin")
     emails = @recipients.collect(&:email).join(",")
+    @root_url = root_url
     mail(to: emails, subject: "Alert Additional Content Has Been Reported!")
   end
   def reset_password(user,token)
     @user = User.find(user)
     @token = token
+    @root_url = root_url
     mail(to: @user.email, subject: "Reset Geofocus Password.")
   end
 end
