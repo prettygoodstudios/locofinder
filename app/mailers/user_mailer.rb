4f84723b3c32989791ed1be0352112989ee2c76c
Miguel Rust <hava12soft@gmail.com>
2018-03-20 20:37:53 -0600

    This should fix the issue.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'info@geofoc.us'
   def verify_email(user)
     @user = User.find(user)
-    @root_url = root_url
+    @root_url = "https://geofocus.herokuapp.com/"
     mail(to: @user.email, subject: 'Geofocus Email Verification')
   end
   def new_report(report,user)
@@ -10,13 +10,13 @@ class UserMailer < ApplicationMailer
     @user = User.find(user)
     @recipients = User.all.where(role: "admin")
     emails = @recipients.collect(&:email).join(",")
-    @root_url = root_url
+    @root_url = "https://geofocus.herokuapp.com/"
     mail(to: emails, subject: "Alert Additional Content Has Been Reported!")
   end
   def reset_password(user,token)
     @user = User.find(user)
     @token = token
-    @root_url = root_url
+    @root_url = "https://geofocus.herokuapp.com/"
     mail(to: @user.email, subject: "Reset Geofocus Password.")
   end
 end
