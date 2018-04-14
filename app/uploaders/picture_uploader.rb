291587a00dfe2e12d9fb2204609643bdb7a11662
Miguel Rust <hava12soft@gmail.com>
2018-04-14 16:43:36 -0600

    Fixed cropit js x-if data issues.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,6 +18,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  def extension_white_list
+    %w(jpg jpeg png gif)
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -10,12 +10,19 @@ class ProfileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  def auto_orient
+    manipulate! do |img|
+      img.auto_orient
+      img
+    end
+  end
   def store_dir
     "uploads/profiles/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   def extension_white_list
     %w(jpg jpeg png gif)
   end
+  process :auto_orient
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
