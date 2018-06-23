1b04fb33fd04e12dcb07e629e3df591e6fbe3e14
Miguel Rust <hava12soft@gmail.com>
2018-06-23 14:45:34 -0600

    Forgot to precompile.

---
+++ public/assets/alerts-3383af351e5ddb7a2c686bf792692ae3fcd64fd0ab56e09eaddf81412509784a.js
@@ -0,0 +1,38 @@
+$( document ).on('turbolinks:load', function() {
+  var show = true;
+  var lastWidth = window.innerWidth;
+  $("#toggle").click(function(){
+    if(show){
+      show = false;
+      $(".nav ul").show();
+      $("#toggle").addClass("is-active");
+    }else{
+      show = true;
+      $(".nav ul").hide();
+      $("#toggle").removeClass("is-active");
+    }
+  });
+  var content = $.trim( $('.modal-body').text() ).length;
+  console.log(content);
+  if(content != 0){
+    console.log("Showwing");
+    $(".modal").show();
+  }
+  $(window).click(function(){
+    $(".modal").hide();
+  });
+  $(".close").click(function(){
+    $(".modal").hide();
+  });
+  $(window).resize(function(){
+    if (window.innerWidth < 500 && lastWidth > 500){
+      $(".nav ul").hide();
+      $(".hamburger").removeClass("is-active");
+      show = true;
+    }else if (window.innerWidth > 500){
+      $(".nav ul").css("display","inline");
+      $(".hamburger").removeClass("is-active");
+    }
+    lastWidth = window.innerWidth;
+  });
+});
