67f65ab18ad784626939074748cf32f9e8fcd33f
Miguel Rust <hava12soft@gmail.com>
2018-07-14 15:44:43 -0600

    Should now use https as default.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'application'
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def default_url_options(options={})
+   { :protocol => "https" }
+  end
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |user|
