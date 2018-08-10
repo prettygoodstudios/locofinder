3c2c67313cfae63226ba02401e4ca3c974d0a944
Miguel Rust <hava12soft@gmail.com>
2018-08-09 18:11:28 -0600

    Built out comprehensive image resizing algorithim.

--- app/javascript/components/Collection.js
+++ app/javascript/components/Collection.js
@@ -182,23 +182,67 @@ const Grid = (props) => {
     </LazyLoad>
   );
 }
-const Photo = (props) => {
-  var scaleRatio = 0.625;
-  var finalWidth = props.width*props.zoom*scaleRatio;
-  var finalHeight = props.height*props.zoom*scaleRatio;
-  var finalOffsetX = props.offsetX*scaleRatio;
-  var finalOffsetY = props.offsetY*scaleRatio;
-  let totWidth = window.innerWidth * 0.85;
-  let diff = totWidth - (finalWidth - finalOffsetX);
-  console.log("Total Width",totWidth);
-  console.log(diff);
-  if(finalWidth - finalOffsetX < totWidth && window.innerWidth < 550){
-    finalOffsetX +=  totWidth - (finalWidth - finalOffsetX);
-    console.log("Total Width",totWidth);
+class Photo extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      finalWidth: 0,
+      finalHeight: 0,
+      finalOffsetX: 0,
+      finalOffsetY: 0
+    }
   }
-  return(
-    <img style={{width: finalWidth,height: finalHeight,marginLeft: finalOffsetX,marginTop: finalOffsetY}} src={props.url}/>
-  );
+
+  componentDidMount(){
+    this.updateDimensions();
+    window.addEventListener("resize", () =>  this.updateDimensions());
+  }
+
+  updateDimensions = () => {
+    const props = this.props;
+    console.log("My props",props);
+    var scaleRatio = 0.625;
+    var finalWidth = props.width*props.zoom*scaleRatio;
+    var finalHeight = props.height*props.zoom*scaleRatio;
+    var finalOffsetX = props.offsetX*scaleRatio;
+    var finalOffsetY = props.offsetY*scaleRatio;
+    let totWidth = window.innerWidth * 0.85;
+    if(window.innerWidth > 550){
+      totWidth = (window.innerWidth*0.8-window.innerWidth*0.04)/2;
+      if(window.innerWidth > 800){
+        totWidth = (window.innerWidth*0.8-window.innerWidth*0.08)/3;
+      }
+    }
+    let diff = totWidth - (finalWidth - Math.abs(finalOffsetX));
+    console.log("Total Width",totWidth);
+    console.log("Real Width", finalWidth - finalOffsetX);
+    console.log(diff);
+    if(diff > 0){
+      //finalOffsetX +=  totWidth - (finalWidth - finalOffsetX);
+      const scaleY = 250/(finalHeight-Math.abs(finalOffsetY));
+      const scaleX = totWidth/(finalWidth-Math.abs(finalOffsetX));
+      const scaleUp =  scaleY > scaleX ? scaleY : scaleX;
+      finalOffsetX *= scaleUp;
+      finalOffsetY *= scaleUp;
+      finalWidth *= scaleUp;
+      finalHeight *= scaleUp;
+      console.log("Final Width", finalWidth - finalOffsetX);
+    }
+    this.setState({
+      finalWidth,
+      finalHeight,
+      finalOffsetX,
+      finalOffsetY
+    });
+  }
+
+  render(){
+    const {finalWidth, finalHeight, finalOffsetX, finalOffsetY} = this.state;
+    const {props} = this;
+    return(
+      <img style={{width: finalWidth,height: finalHeight,marginLeft: finalOffsetX,marginTop: finalOffsetY}} src={props.url}/>
+    );
+  }
 }
 const UserTag = ({rootUrl, id, profileImg, display, width, height, zoom, offsetX, offsetY}) => {
   return(
