d88eb1ea7a607d7685f578479de67a1bccb1ac1e
Miguel Rust <hava12soft@gmail.com>
2018-08-13 21:41:55 -0600

    Built out location update API.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::LocationsController < ApiController
-  before_action :is_authencticated, only: [:create]
+  before_action :is_authencticated, only: [:create, :update]
+  before_action :set_location, only: [:show, :update]
+  before_action :is_mine, only: [:update]
 
   def index
     @locations = Location.all
@@ -7,13 +9,20 @@ class Api::V1::LocationsController < ApiController
   end
 
   def show
-    @location = Location.find(params[:id])
     render json:  { location: @location, photos: @location.photos }
   end
 
   def create
     @location = @user.locations.create(location_params)
     if @location.save
+      render json: @location
+    else
+      render json: @location.errors
+    end
+  end
+
+  def update
+    if @location.update_attributes(location_params)
       render json: @location
     else
       render json: @location.errors
@@ -33,5 +42,15 @@ class Api::V1::LocationsController < ApiController
       else
         @user = User.where("email = '#{params[:email]}'").first
       end
+    end
+
+    def is_mine
+      if @user.id.to_i != @location.user_id.to_i
+        head(:unauthorized)
+      end
+    end
+
+    def set_location
+      @location = Location.find(params[:id])
     end
 end
