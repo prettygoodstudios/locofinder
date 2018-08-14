27c1738e0b0da9e4d8fee3af98776137720ea952
Miguel Rust <hava12soft@gmail.com>
2018-08-13 21:19:51 -0600

    Built out photo upload api.

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,10 +1,38 @@
 class Api::V1::PhotosController < ApiController
+  before_action :is_authencticated
 
   def show
     @photo = Photo.find(params[:id])
     render json: @photo
   end
 
+  def create
+    @location = Location.find(params[:location].to_i)
+    @photo = @location.photos.build(photo_params)
+    @user.photos << @photo
+    if @photo.save
+      @photo.update_attribute("views",0)
+      @photo.update_attribute("width",@photo.img_url.width)
+      @photo.update_attribute("height",@photo.img_url.height)
+      render json: @photo
+    else
+      render json: { errors: ["Could not save photo."]}
+    end
+  end
+
   private
+
+    def photo_params
+      params.permit(:img_url,:caption,:width,:height,:zoom,:offsetX,:offsetY)
+    end
+
+    def is_authencticated
+      @user = nil
+      if !User.authenticate_via_token(params[:email], params[:token])
+        head(:unauthorized)
+      else
+        @user = User.where("email = '#{params[:email]}'").first
+      end
+    end
 
 end
