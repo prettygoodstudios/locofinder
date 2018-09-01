5ced6d00c96945eddf6f560e3647891d62ac6e28
Miguel Rust <hava12soft@gmail.com>
2018-08-31 22:27:48 -0600

    Refactored locations API V1 controller return location reviews.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -9,23 +9,13 @@ class Api::V1::LocationsController < ApiController
   end
 
   def show
-    ph = @location.photos.mostViews
-    users = User.joins(:photos).where("photos.location_id = #{@location.id}").order("photos.views DESC")
-    @photos = ph.zip(users).map do |p, u|
-      { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
-    end
-    render json:  { location: @location, photos: @photos }
+    return_location_show @location
   end
 
   def create
     @location = @user.locations.create(location_params)
     if @location.save
-      ph = @location.photos.mostViews
-      users = User.joins(:photos).where("photos.location_id = #{@location.id}").order("photos.views DESC")
-      @photos = ph.zip(users).map do |p, u|
-        { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
-      end
-      render json:  { location: @location, photos: @photos }
+      return_location_show @location
     else
       render json: { errors: @location.errors }
     end
@@ -33,12 +23,7 @@ class Api::V1::LocationsController < ApiController
 
   def update
     if @location.update_attributes(location_params)
-      ph = @location.photos.mostViews
-      users = User.joins(:photos).where("photos.location_id = #{@location.id}").order("photos.views DESC")
-      @photos = ph.zip(users).map do |p, u|
-        { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
-      end
-      render json:  { location: @location, photos: @photos }
+      return_location_show @location
     else
       render json: { errors: @location.errors }
     end
@@ -57,6 +42,20 @@ class Api::V1::LocationsController < ApiController
       else
         @user = User.where("email = '#{params[:email]}'").first
       end
+    end
+
+    def return_location_show location
+      ph = location.photos.mostViews
+      users = User.joins(:photos).where("photos.location_id = #{location.id}").order("photos.views DESC")
+      photos = ph.zip(users).map do |p, u|
+        { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
+      end
+      rv_users = User.find_by_sql("SELECT u.* FROM users u JOIN reviews r ON r.user_id = u.id WHERE r.location_id = #{location.id} ORDER BY r.score DESC")
+      rv = Review.find_by_sql("SELECT r.* FROM reviews r JOIN users u ON r.user_id = u.id WHERE r.location_id = #{location.id} ORDER BY r.score DESC")
+      reviews = rv.zip(rv_users).map do |r, u|
+        { id: r.id, score: r.score, message: r.message ,user_id: r.user_id, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
+      end
+      render json:  { location: location, photos: photos, reviews: reviews }
     end
 
     def is_mine
