d425a720e561fc6d35efda631f48907455f336b4
Miguel Rust <hava12soft@gmail.com>
2018-08-31 18:23:41 -0600

    Refactored the update action in the locations controller API V1.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -33,9 +33,14 @@ class Api::V1::LocationsController < ApiController
 
   def update
     if @location.update_attributes(location_params)
-      render json: @location
+      ph = @location.photos.mostViews
+      users = User.joins(:photos).where("photos.location_id = #{@location.id}").order("photos.views DESC")
+      @photos = ph.zip(users).map do |p, u|
+        { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
+      end
+      render json:  { location: @location, photos: @photos }
     else
-      render json: @location.errors
+      render json: { errors: @location.errors }
     end
   end
 
