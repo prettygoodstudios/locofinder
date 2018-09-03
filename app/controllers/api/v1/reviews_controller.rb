1c140eb605484e6ebaf4b2c9456210c78c622211
Miguel Rust <hava12soft@gmail.com>
2018-09-02 18:50:39 -0600

    Refactored reviews controller to return the whole location for the update and create actions.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -27,12 +27,18 @@ class Api::V1::ReviewsController < ApiController
     end
 
     def render_reviews
-      reviews = Review.where("location_id = #{params[:location_id]}").order("id DESC")
-      users = User.joins(:reviews).where("reviews.location_id = #{params[:location_id]}").order("reviews.id DESC")
-      @reviews = reviews.zip(users).map do |r, u|
+      location = Location.find(params[:location_id])
+      ph = location.photos.mostViews
+      users = User.joins(:photos).where("photos.location_id = #{location.id}").order("photos.views DESC")
+      photos = ph.zip(users).map do |p, u|
+        { id: p.id, zoom: p.zoom, offsetX: p.offsetX, offsetY: p.offsetY, width: p.width, height: p.height, caption: p.caption, user_id: p.user_id, img_url: p.img_url.url, views: p.views, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
+      end
+      rv_users = User.find_by_sql("SELECT u.* FROM users u JOIN reviews r ON r.user_id = u.id WHERE r.location_id = #{location.id} ORDER BY r.score DESC")
+      rv = Review.find_by_sql("SELECT r.* FROM reviews r JOIN users u ON r.user_id = u.id WHERE r.location_id = #{location.id} ORDER BY r.score DESC")
+      reviews = rv.zip(rv_users).map do |r, u|
         { id: r.id, score: r.score, message: r.message ,user_id: r.user_id, user_display: u.display, user_profile: u.profile_img.url, user_zoom: u.zoom, user_width: u.width, user_height: u.height, user_offsetX: u.offsetX, user_offsetY: u.offsetY}
       end
-      render json: @reviews
+      render json:  { location: location, photos: photos, reviews: reviews }
     end
 
     def set_review
