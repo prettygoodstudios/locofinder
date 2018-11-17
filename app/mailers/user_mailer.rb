183ddce35f0fb996c68ef5fe381dde1692a30e3c
Miguel Rust <hava12soft@gmail.com>
2018-11-16 23:21:51 -0700

    Fixed issues regarding verifying email and reseting password.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def verify_email(user)
     @user = User.find(user)
     @token = @user.token
-    @root_url = "https://geofocus.herokuapp.com/"
+    @root_url = "https://geofoc.us/"
     mail(to: @user.email, subject: 'Geofocus Email Verification')
   end
   def new_report(report,user)
@@ -11,13 +11,13 @@ class UserMailer < ApplicationMailer
     @user = User.find(user)
     @recipients = User.all.where(role: "admin")
     emails = @recipients.collect(&:email).join(",")
-    @root_url = "https://geofocus.herokuapp.com/"
+    @root_url = "https://geofoc.us/"
     mail(to: emails, subject: "Alert Additional Content Has Been Reported!")
   end
   def reset_password(user,token)
     @user = User.find(user)
     @token = token
-    @root_url = "https://geofocus.herokuapp.com/"
+    @root_url = "https://geofoc.us/"
     mail(to: @user.email, subject: "Reset Geofocus Password.")
   end
 end
